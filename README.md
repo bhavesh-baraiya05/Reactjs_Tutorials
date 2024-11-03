9abc0273bdcc607d6ccb8bbd0d85bcf910055bea
Bhavesh <bhaveshbaraiya5555@gmail.com>
2024-11-03 18:53:43 +0530

    First version of learning React from Angela Yu's Udemy WebD course.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Reactjs_Tutorials
+
+It is a JavaScript library for building user interface
---
+++ my-react-app/src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<div>
+    <h1>My Favorite Foods</h1>
+    <ul title="Frameworks">
+        <li>Jalebi</li>
+        <li>Fafda</li>
+        <li>Gulab Jamoon</li>
+    </ul>
+</div>, document.getElementById("root"));
